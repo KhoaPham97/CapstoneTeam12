79061d3c236561a921deaff2db7f3528a904580e
KhoaPham <phamdangkhoa0403@gmail.com>
2018-10-26 22:25:06 +0700

    Up file

---
+++ Capstone/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capstone.Models;
+
+namespace Capstone.Controllers
+{
+    public class LoginController : Controller
+    {
+        CapstoneEntities model = new CapstoneEntities();
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            ViewBag.mgs = "";
+            var user = model.Users.FirstOrDefault(x => x.Username== username);
+      
+                  return RedirectToAction("Index", "User");
+        }
+         
+
+       
+	}
+
+    }
+
